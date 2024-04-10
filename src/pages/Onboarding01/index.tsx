1224d40daba50c28c2b79cab332f84fd20e99896
Evandro <evandro.gomes@actionsys.com.br>
2024-04-10 11:01:05 -0300

    temp commit

---
+++ src/pages/Onboarding01/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import React from "react"
+export default function onBoarding01()  {
+
+  return( 
+    <>
+    <img src="assets/onBoarding/Group 28.png" alt="characters" />
+    <h1>Todos os Pokémons em um só Lugar</h1>
+    <p>Acesse uma vasta lista de Pokémon de 
+todas as gerações já feitas pela Nintendo</p>
+    <Button aria-atomic aria-valuetext="e"/>
+    </>
+    )
+}
