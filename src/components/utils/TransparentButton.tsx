715eeef25125bdf7e27376855a763e4da5c8ae42
Evandro <evandro.gomes@actionsys.com.br>
2024-04-26 09:46:17 -0300

    atualizado layout tela login e botão translucido

--- src/components/utils/TransparentButton.tsx
+++ src/components/utils/TransparentButton.tsx
@@ -3,15 +3,17 @@ import Button from "@mui/material/Button";
 interface TransparentButtonProps {
   text: string;
   icon?: string; // URL da imagem
+  width?: number;
+  height?: number;
 }
 
-const TransparentButton: React.FC<TransparentButtonProps> = ({ text, icon }) => {
+const TransparentButton: React.FC<TransparentButtonProps> = ({ text, icon, width, height }) => {
   return (
     <Button
       variant="outlined"
       className="rounded-full w-full h-14 normal-case text-lg text-gray-600 border-gray-500 bg-transparent border-3"
       // eslint-disable-next-line @next/next/no-img-element
-      startIcon={icon && <img src={icon} alt="icon" style={{ marginRight: 8, width: 24, height: 24 }} />}
+      startIcon={icon && <img src={icon} alt="icon" style={{ marginRight: 8, width: width === undefined ? 24 : width, height: height === undefined ? 24 : height }} />}
     >
       {text}
     </Button>
--- src/pages/LoginEscolha/index.tsx
+++ src/pages/LoginEscolha/index.tsx
@@ -8,34 +8,30 @@ import TransparentButton from "@/components/utils/TransparentButton";
 
 export default function LoginEscolha() { 
   return (
-    <>
-      <div className="h-screen flex flex-col">
+      <div className="h-screen flex flex-col gap-4 text-center justify-center">
         <header className="justify-center flex">
-          <div className="text-center">
-          <div className="flex text-center justify-center py-5">
-            <ChevronLeft className="flex absolute top-5 left-0 "/>
-          <h1 className="font-semibold text-base text-center">Entrar</h1>
+          <div className="flex gap-36 py-5 absolute top-5 left-0">
+            <ChevronLeft />
+          <h1 className="font-semibold text-base ">Entrar</h1>
           </div>
         <img
                 width={280}
                 src="assets/EscolhaLogin/Group_39.png"
                 alt="characters"
               />
-              </div>
         </header>
         <div className="">
-          <h2 className="text-3xl text-center font-semibold"> Que bom te ver aqui novamente!</h2>
-          <p className="text-center text-gray-600 mt-2">Como deseja se conectar?</p>
-          <div className="flex flex-col gap-3 mt-4">
+            <h2 className="text-3xl  font-semibold"> Que bom te ver aqui novamente!</h2>
+            <p className=" text-gray-600 mt-2">Como deseja se conectar?</p>
+          <div className="flex flex-col gap-3 mt-14">
           
           <TransparentButton text="Continuar com o Google" icon="assets\icons\imgGoogle.png" />
-          <TransparentButton text="Continuar com o GitHub" icon="assets\icons\Vector.png" />
+          <TransparentButton text="Continuar com o GitHub" icon="assets\icons\Vector.png" width={20} />
 
           {BlueButton('Continuar com um e-mail')}
           </div>
 
         </div>
       </div>
-    </>
   );
 }
